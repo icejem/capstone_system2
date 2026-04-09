da8f6947d743c7c629576f2544c9433cbe40ea70
icejem <awesomejm12@gmail.com>
2026-04-10 07:58:23 +0800

    reset

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,28 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div
+        {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600 transition-opacity duration-500']) }}
+        data-auth-session-status
+    >
         {{ $status }}
     </div>
+
+    <script>
+        (() => {
+            const status = document.querySelector('[data-auth-session-status]');
+
+            if (!status) {
+                return;
+            }
+
+            window.setTimeout(() => {
+                status.classList.add('opacity-0');
+
+                window.setTimeout(() => {
+                    status.remove();
+                }, 500);
+            }, 4000);
+        })();
+    </script>
 @endif
